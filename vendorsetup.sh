66722fdd9b4e7634cfaccec1c79d3348608d8d06
OLIMPMO5 <yaroslav.malykh@gmail.com>
2021-02-15 15:14:17 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/DespairFactor/aarch64-linux-android-6.x prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-6.x
+git clone https://gitlab.com/matthewalex/aarch64-linux-android-6.x-kernel.git prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-6.x
