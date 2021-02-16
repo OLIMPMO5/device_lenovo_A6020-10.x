a16f0afc1844a3cf762b0b1ec06ba2e820fe0688
OLIMPMO5 <yaroslav.malykh@gmail.com>
2021-02-16 20:56:52 +0400

    Gapps not included

--- aosp_A6020.mk
+++ aosp_A6020.mk
@@ -43,8 +43,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 # Set this flag in build script
 #ifeq ($(CURRENT_BUILD_TYPE), gapps)
 #$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
-TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
+#TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+#WITH_GAPPS := true
+#TARGET_GAPPS_ARCH := arm64
+#IS_PHONE := true
 #endif
